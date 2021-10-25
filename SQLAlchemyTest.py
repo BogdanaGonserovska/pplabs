605d5ddd210dfda694373b3f8959ac60faa4f6e3
Iryna Koval <iryna.koval2003@gmail.com>
2021-10-25 09:59:16 +0300

    SQLAlchemy sample script

--- SQLAlchemyTest.py
+++ SQLAlchemyTest.py
@@ -8,26 +8,26 @@ session = Session()
 
 from model import User
 
-user = User(
-    username = "Tom2",
-    firstName = "Tom",
-    lastName ="Jerry",
-    email = "tom@gmail.com",
-    password = "ttttom",
-    phone = "+23 0394756182",
-    isModerator = True,
-    isActive = False)
+user = session.query(User).filter(User.username=="Tom2").one_or_none()
 
+if (user == None):
+    user = User(
+        username = "Tom2",
+        firstName = "Tom",
+        lastName ="Jerry",
+        email = "tom@gmail.com",
+        password = "ttttom",
+        phone = "+23 0394756182",
+        isModerator = True,
+        isActive = False)
+    session.add(user)
+    session.commit()
 
-session.add(user)
-session.commit()
-
-user_from_db = session.query(User).filter(User.id==user.id).one_or_none()
-user_from_db.lastName = "Smith"
+user.lastName = "Smith"
 session.commit()
 
 slovo ="ith"
 from sqlalchemy import or_
 list = session.query(User).filter(or_(User.username.like('%'+slovo+'%'),User.lastName.like('%'+slovo+'%'))).all()
 for y in list:
-    print(y.password)
+    print(y.lastName, y.password)
